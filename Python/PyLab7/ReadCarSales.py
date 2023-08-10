13f8da0d835b3bb003f20063aac321cf0bc524ae
Eloise <Eloise@STUDENT03>
2023-08-10 16:51:49 +0100

    Completed PyLab7!

--- Python/PyLab7/ReadCarSales.py
+++ Python/PyLab7/ReadCarSales.py
@@ -5,18 +5,29 @@ def listToNums(salesList):
 		newList.append(int(l))
 	return newList
 
-#lists
+#declaring the lists
 companies = []
 sales = []
 janSales = []
+febSales = []
+marSales = []
+aprSales = []
+maySales = []
+junSales = []
+julSales = []
+augSales = []
+
+bmwSales = []
+vauxhallSales = []
+mercSales = []
+vwSales = []
+fordSales = []
+
 #opens and reads file
 with open("Python/PyLab7/carSale.csv","r") as openedFileVar:
-	fileAsAString = openedFileVar.readlines() 
+	fullList = openedFileVar.readlines()
 
-	#print(fileAsAString)
-	#stringToList = fileAsAString.split(',')
-
-	for x in fileAsAString[1:8]:
+	for x in fullList[1:8]:
 		#companies.append(list(map(str,stringToList[0])))
 		stringToList = x.split(",")
 		companies.append(stringToList[0])
@@ -29,7 +40,8 @@ with open("Python/PyLab7/carSale.csv","r") as openedFileVar:
 		junSales.append(stringToList[6])
 		julSales.append(stringToList[7])
 		augSales.append(stringToList[8])
-
+		sales.append(listToNums(stringToList[1:9]))
+		
 	#loop over each sales list to create a new list
 	januaryInts = listToNums(janSales)
 	februaryInts = listToNums(febSales)
@@ -40,19 +52,50 @@ with open("Python/PyLab7/carSale.csv","r") as openedFileVar:
 	julyInts = listToNums(julSales)
 	augustInts = listToNums(augSales)
 	
-	#janSalesNumber = janSales.remove()
-	#janTotal = sum(janSalesNumber)
-	print(januaryInts)
-	#for l in fileAsAString:
-		#print(fileAsAString)
-		#manufacturer sales
-		#bmwSales = stringToList[0:7]
-		#vauxhallSales = stringToList[1:7]
-		#mercSales = stringToList[2]
-		#vwSales = stringToList[3]
-		#fordSales = stringToList[4]
+	#summing the totals
+	janTotal = sum(januaryInts)
+	febTotal = sum(februaryInts)
+	marTotal = sum(marchInts)
+	aprTotal = sum(aprilInts)
+	mayTotal = sum(mayInts)
+	junTotal = sum(juneInts)
+	julTotal = sum(julyInts)
+	augTotal = sum(augustInts)
+	print("\n") 
+	print("--- Monthly sales ---")
+	print(" The Sum of cars sold in January is",janTotal)
+	print(" The Sum of cars sold in February is",febTotal)
+	print(" The Sum of cars sold in March is",marTotal)
+	print(" The Sum of cars sold in April is",aprTotal)
+	print(" The Sum of cars sold in May is",mayTotal)
+	print(" The Sum of cars sold in June is",junTotal)
+	print(" The Sum of cars sold in July is",julTotal)
+	print(" The Sum of cars sold in August is",augTotal)
+	print("\n")
+	#Working out how to find total yearly sales by manufacturer
+
+	bmwSales =(sales[4])
+	vauxhallSales = (sales[3])
+	mercSales = (sales[2])
+	vwSales = (sales[1])
+	fordSales = (sales[0])
 		#print(companies)
-		#print(vauxhallSales)
+	
 
-
+	#summing the totals
+	fordTotal = sum(fordSales)
+	vwTotal = sum(vwSales)
+	mercTotal = sum(mercSales)
+	vauxhallTotal = sum(vauxhallSales)
+	bmwTotal = sum(bmwSales)
+	print("--- Yearly Totals ---")
+	print(" The yearly sales total for Ford Motor Company is",fordTotal)
+	print(" The yearly sales total for Volkswagen UK is",vwTotal)
+	print(" The yearly sales total for Mercedes-Benz UK is",mercTotal)
+	print(" The yearly sales total for Vauxhall Motors is",vauxhallTotal)
+	print(" The yearly sales total for BMW is",bmwTotal)
+	print("\n")
+	
+	
+	
 	
